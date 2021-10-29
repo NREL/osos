b3a656ca7032c96aa7feeca65534dfec81953fac
grantbuster <grant.buster@nrel.gov>
2021-10-29 09:43:56 -0600

    fixed conda build shell issues

--- conda_build.sh
+++ conda_build.sh
@@ -6,8 +6,8 @@ export CONDA_BLD_PATH=~/conda-bld
 
 conda build conda.recipe/ --channel=nrel
 
-anaconda upload -u nrel $(conda build conda.recipe/ --channel=nrel --output)
+anaconda upload -u nrel "$(conda build conda.recipe/ --channel=nrel --output)"
 
 echo "Building and uploading conda package done!"
-rm -rf $CONDA_BLD_PATH/*
+rm -rf "${CONDA_BLD_PATH:?}/"*
 ls $CONDA_BLD_PATH
